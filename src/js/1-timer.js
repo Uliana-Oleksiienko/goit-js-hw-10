8140278f92dac21092d39ef3ad6daf362a4613b5
ulyaleks <ulyaleksss@gmail.com>
2025-08-04 19:20:17 +0200

    timer fixed

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,11 +1,6 @@
-// Описаний в документації
 import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
-
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
 const input = document.querySelector('#datetime-picker');
@@ -27,6 +22,7 @@ const options = {
   onClose(selectedDates) {
     const selectedDate = selectedDates[0];
     const currentDate = new Date();
+    
     if (selectedDate <= currentDate) {
       iziToast.error({
         title: 'Error',
@@ -41,7 +37,7 @@ const options = {
   },
 };
 
-flatpickr(input, options);
+const fp = flatpickr(input, options);
 
 startButton.addEventListener('click', () => {
   startButton.disabled = true;
@@ -54,10 +50,15 @@ startButton.addEventListener('click', () => {
     if (diff <= 0) {
       clearInterval(timerId);
       updateTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-      input.disabled = false; // Активуємо поле вводу після завершення
+      
+      // Полная перезагрузка flatpickr
+      fp.destroy();
+      input.disabled = false;
+      flatpickr(input, options);
+      
       iziToast.success({
         title: 'Done',
-        message: 'Timer finished!',
+        message: 'Timer finished! Choose a new date',
         position: 'topRight',
       });
       return;
@@ -80,19 +81,14 @@ function addLeadingZero(value) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
